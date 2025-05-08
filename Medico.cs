d0f4df468da67cb8ef6ee7314365c646a4243a3f
juan <juan@shaper.us>
2025-05-07 22:41:42 -0300

    add feature: class Medics

---
+++ Medico.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_3_2_Clinica
+{
+    internal class Medico : Persona
+    {
+
+        // Atributes
+        protected string Especialidad;
+        protected double CostoConsulta;
+
+        // Constructor
+        public Medico() : base()
+        {
+            this.Especialidad = string.Empty;
+            this.CostoConsulta = 0;
+        }
+        public Medico(string nombre, int DNI, string telefono, bool sexo, string obraSocial, double costoConsulta) : base(nombre, DNI, telefono, sexo)
+        {
+            this.Especialidad = obraSocial;
+            this.CostoConsulta = costoConsulta;
+        }
+
+        // Properties
+        public string especialidad
+        {
+            get { return this.Especialidad; }
+            set { this.Especialidad = value; }
+
+        }
+        public double costoConsulta
+        {
+            get { return this.CostoConsulta; }
+            set { this.CostoConsulta = value; }
+
+        }
+
+        // Method
+        public override string ToString()
+        {
+            return base.ToString() + $" {especialidad}, {costoConsulta}";
+        }
+    }
+}
