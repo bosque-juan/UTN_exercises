c4995b1e20f8bb30ecfac8669b4a070cae56788e
juan <juan@shaper.us>
2025-07-06 15:40:57 -0300

    SiloSoltion

---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_2_2_silo/Ej2.2_silo/Ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ej2_2_Silo.Silo;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido ! para calcular el volumen total del silo por favor ingrese:");
+        //solicita valores del constructor
+
+        Console.WriteLine("Altura del cuerpo del silo:");
+        double Altura = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Perfecto, ahora ingrese altura del techo del silo:");
+        double AlturaTecho = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Por último ingrese altura del radio del silo:");
+        double Radio = Convert.ToDouble(Console.ReadLine());
+        Ej_2_Silos.Silo SiloVolume = new(Altura, AlturaTecho, Radio);
+        string VolumeSilo = Math.Round(SiloVolume.VolumnenSilo,2).ToString();
+        Console.WriteLine($" El volumen total del Silo es de {VolumeSilo} metros cúbicos");
+    }
+}
