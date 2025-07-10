5bce262ab03a943165ce0506685992bfaeb2b265
juan <juan_bosque@hotmail.com>
2025-07-10 13:04:16 -0300

    modeling a dot trhough x and y axis

---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicio_Unidad_2/Ej_2_3_Punto/Punto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_2_3_Punto
+{
+    internal class Punto
+    {
+        private double EjeX = 0;
+        private double EjeY = 0;
+
+        public Punto() { }
+
+        public Punto(double x, double y)
+        {
+            this.EjeX = x;
+            this.EjeY = y;
+        }
+
+        public string PuntoCoordenada()
+        { 
+            return $"{EjeX};{EjeY}"; 
+        }
+
+        public string DistanciaAlOrigen()
+        {
+            return $"La distancia al origen es: {(Math.Sqrt(Math.Pow(EjeX,2) + Math.Pow(EjeY,2))).ToString()}";
+        }
+    }
+}
