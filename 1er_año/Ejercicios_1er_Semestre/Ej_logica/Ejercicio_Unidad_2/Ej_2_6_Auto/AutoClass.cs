8249e7678a9f06aa3217f35c2bb7e7eae4470048
juan <juan_bosque@hotmail.com>
2025-07-14 12:10:23 -0300

    feat: class car, method drive, recharge oil

---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicio_Unidad_2/Ej_2_6_Auto/AutoClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_2_6_Auto
+{
+    internal class AutoClass
+    {
+        // Atributos 
+        private double CombustibleTanque = 0;
+        // Atributos pre seteados para el conductor
+        private int CapacidadTanque = 54;
+        private double Autonomia = 11;
+
+
+        // Constructores
+        public AutoClass() {  }
+
+        public AutoClass(double CombustibleTanque)
+        {
+            this.CombustibleTanque = CombustibleTanque;
+        }
+
+
+        // Propiedades
+        public double pCombustibleTanque
+        {
+            get { return CombustibleTanque; }
+            set { CombustibleTanque = value; }
+        }
+
+        public int pCapacidadTanque
+        {
+            get { return CapacidadTanque; }
+            set { CapacidadTanque = value; }
+        }
+
+        public double pAutonomia
+        {
+            get { return Autonomia; }
+            set { Autonomia = value; }
+        }
+
+
+        // Método Conducir
+        public void Conducir(double kilometrosRecorrer)
+        {
+            double AutonomiaDisponible = pCombustibleTanque * pAutonomia;
+            if (AutonomiaDisponible >= kilometrosRecorrer)
+            {
+                double litrosAconsumir = kilometrosRecorrer / 11;
+                Console.WriteLine("litros disponibles en el tanque :" + pCombustibleTanque);
+                Console.WriteLine("Con el combustible disponible, el viaje puede ser completado");
+                pCombustibleTanque = pCombustibleTanque - litrosAconsumir;
+                Console.WriteLine("litros disponibles en el tanque luego del recorrido:" + pCombustibleTanque + " presione enter para siguiente operación");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No hay suficientemente combusitble en el tanque para recorrer el viaje. Recargar combustible.");
+                Console.ReadKey();
+            };
+        }
+
+        // Metodo Cargar Combustible
+        public void CargarCombustible(double litros)
+        {
+            // Calcula derrame
+            double totalPretendidoCargar = pCombustibleTanque + litros;
+            double derrame = pCapacidadTanque - totalPretendidoCargar;
+
+            // Carga combustible
+            pCombustibleTanque += litros;
+            if (derrame <= 0)
+            {
+                // Resta derrame
+                pCombustibleTanque += derrame;
+
+                Console.WriteLine($"Combustible derramado {derrame.ToString()} litros, presione enter para siguiente operación");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"Combustible disponible previo a la carga: {pCombustibleTanque} litros.");
+                Console.WriteLine($"Combustible cargado exitosamente, {pCombustibleTanque} litros en tanque presione enter para siguiente operación");
+                Console.ReadKey();
+            };
+
+        }
+    }
+}
---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicio_Unidad_2/Ej_2_6_Auto/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ej_2_6_Auto
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AutoClass AutoNaftero = new AutoClass(10);
+
+            AutoNaftero.CargarCombustible(50);
+
+            AutoNaftero.Conducir(111);
+        }
+    }
+}
