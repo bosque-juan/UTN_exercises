9eeccc99678efff57a1941475cd2a65f5651f28e
juan <juan_bosque@hotmail.com>
2025-07-18 16:12:39 -0300

    Add monthly sales matrix by branch and totals per month and per branch

---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicio_Unidad_2/Ej_9_tabla_ingresos/Persona_practica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_9_tabla_ingresos
+{
+    internal class Persona_practica
+    {
+
+
+        public string? nombre;
+        public string? apellido;
+        public Persona_practica(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public string ShowPerson()
+        {
+            return $"El nombre de la persona es: {nombre}, {apellido}";
+        }
+
+    }
+}
---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicio_Unidad_2/Ej_9_tabla_ingresos/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Ej_9_tabla_ingresos;
+
+Supermercados sucursal1 = new Supermercados(300, 400, 500);
+Supermercados sucursal2 = new(200, 100, 700);
+Supermercados sucursal3 = new Supermercados(5000.78, 200.56, 5000.56);
+double[,] CadenaSupermercado = new double[3, 3]
+{
+    { sucursal1.mes1,sucursal1.mes2,sucursal1.mes3 },
+    { sucursal2.mes1,sucursal2.mes2,sucursal2.mes3 },
+    { sucursal3.mes1,sucursal3.mes2,sucursal3.mes3}
+};
+
+double totalCadenaVenta = 0;
+for (int i = 0; i < CadenaSupermercado.GetLength(0); i++)
+{
+    double totalSucursal = 0;
+    for (int j = 0; j < CadenaSupermercado.GetLength(1); j++)
+    {
+        totalSucursal = totalSucursal + CadenaSupermercado[i, j];
+        Console.Write($"{CadenaSupermercado[i, j]}\t");
+    }
+    Console.WriteLine($"El total vendido por la sucursal{i + 1}, fué de: {totalSucursal}");
+    totalCadenaVenta += totalSucursal;
+}
+Console.WriteLine($"El total vendido por todas las cadenas fué de: {totalCadenaVenta}");
