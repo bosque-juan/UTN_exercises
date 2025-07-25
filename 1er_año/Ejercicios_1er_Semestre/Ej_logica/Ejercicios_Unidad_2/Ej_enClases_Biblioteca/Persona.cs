1e265d241b2b6514c32fea6da49d3f6dc8e02efc
juan <juan_bosque@hotmail.com>
2025-07-25 15:52:24 -0300

    Association excercise, agregation

---
+++ 1er_año/Ejercicios_1er_Semestre/Ej_logica/Ejercicios_Unidad_2/Ej_enClases_Biblioteca/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public abstract class Persona
+    {
+        protected string nombre;
+        protected int documento;
+        protected string sexo;
+        public Persona()
+        {
+            this.nombre = string.Empty;
+            this.documento = 0;
+            this.sexo = string.Empty;
+        }
+        public Persona(string nombre, int documento, string sexo)
+        {
+            this.nombre = nombre;
+            this.documento = documento;
+            this.sexo = sexo;
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Documento
+        {
+            get { return documento; }
+            set { documento = value; }
+        }
+        public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+        public abstract string MostrarDatos();
+
+
+    }
+}
